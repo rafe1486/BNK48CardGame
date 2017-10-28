857a501b914b847d0a19ade7564c8ede816aa248
rafe1486 <songrit.k@mail.kmutt.ac.th>
2017-10-29 00:35:56 +0700

    first commit with STS

---
+++ src/main/java/com/ip/IndividualProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ip/config/Config.java
@@ -0,0 +1,13 @@
+package com.ip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class Config extends WebMvcConfigurerAdapter {
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+	}
+}
